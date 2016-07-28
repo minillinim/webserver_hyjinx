83d86996e655489f486a741f861dee9882db5b52
minillinim <michael.imelfort@gmail.com>
2016-07-29 00:34:45 +1000

    [MI] Seems to be working now. Containers build and run, kibana is prottected behind a password

---
+++ roles/nginx/templates/nginx.rb
@@ -0,0 +1,1 @@
+rule=:%ip:word% %ruser:word% %auth:word% [%timestamp:char-to:]%] "%method:word% %url:word% %pver:char-to:"%" %status:number% %bytesend:number% "%referer:char-to:"%" "%useragent:char-to:"%"
